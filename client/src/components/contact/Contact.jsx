5bd9226219771627ffb9c060cfc9d84bf7f59d4d
ruthgrg <gurungruth2@gmail.com>
2023-09-03 13:38:09 +0200

    contact getStarted components implemented

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,4 +1,6 @@
 import { MdCall } from 'react-icons/md'
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
 import "./contact.css"
 
 const Contact = () => {
@@ -11,9 +13,9 @@ const Contact = () => {
                 <span className='secondaryText'>e always ready to help by providing the best services for you <br/>
                     we beleiving a good blace to live can make your life better</span>
 
-                <div className="flexColStart contactModes">
-                    <div className="flexColStart row">
-                        {/** First mode */}
+                <div className="contactModes">
+                    {/** First mode */}
+                    <div className="flexStart row">
                         <div className="flexColCenter mode">
                             <div className="flexStart">
                                 <div className="flexStart icon">
@@ -26,46 +28,53 @@ const Contact = () => {
                          </div>
                             <div className="flexCenter button">Call</div>                
                         </div>
-                        {/** Second mode */}
+                    </div>     
+                    {/** Second mode */}
+                    <div className="flexStart row">
                         <div className="flexColCenter mode">
                             <div className="flexStart">
                                 <div className="flexStart icon">
-                                    <MdCall size={25}/>
+                                    <BsFillChatDotsFill size={25}/>
                                 </div>
                                 <div className="flexColStart detail">
-                                <span className='primaryText'>Call</span>
+                                <span className='primaryText'>Chat</span>
                                 <span className='secondaryText'>021 123 456 78</span>
                             </div>
                          </div>
-                            <div className="flexCenter button">Call</div>                
+                            <div className="flexCenter button">Chat</div>                
                         </div>
-                        {/** Third mode */}
+                    </div>
+                    {/** Third mode */}
+                    <div className="flexStart row">
                         <div className="flexColCenter mode">
                             <div className="flexStart">
                                 <div className="flexStart icon">
-                                    <MdCall size={25}/>
+                                    <BsFillChatDotsFill size={25}/>
                                 </div>
                                 <div className="flexColStart detail">
-                                <span className='primaryText'>Call</span>
+                                <span className='primaryText'>Video Call</span>
                                 <span className='secondaryText'>021 123 456 78</span>
                             </div>
                          </div>
-                            <div className="flexCenter button">Call</div>                
+                            <div className="flexCenter button">Video Call</div>                
                         </div>
-                        {/** Fourth mode */}
+                    </div>   
+                    {/** Fourth mode */}
+                    <div className="flexStart row">
                         <div className="flexColCenter mode">
                             <div className="flexStart">
                                 <div className="flexStart icon">
-                                    <MdCall size={25}/>
+                                    <HiChatBubbleBottomCenter size={25}/>
                                 </div>
                                 <div className="flexColStart detail">
-                                <span className='primaryText'>Call</span>
+                                <span className='primaryText'>Message</span>
                                 <span className='secondaryText'>021 123 456 78</span>
                             </div>
                          </div>
-                            <div className="flexCenter button">Call</div>                
+                            <div className="flexCenter button">Message now</div>                
                         </div>
-                    </div> 
+
+                    </div>
                 </div>
             </div>
             <div className="c-right">
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <section className="f-wrapper">
+        <div className="paddings innerWidth flexCenter f-container">
+            {/** left */}
+            <div className="flexColStart f-left">
+                <img src="./logo2" alt="" />
+            </div>
+            {/** right */}
+        </div>
+    </section>
+  )
+}
+
+export default Footer
+
