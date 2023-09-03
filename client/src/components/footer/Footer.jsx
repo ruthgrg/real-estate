1c0356699787541e1319d1a4a435c5eb60771a05
ruthgrg <gurungruth2@gmail.com>
2023-09-03 13:46:29 +0200

    footer component added

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -6,9 +6,23 @@ const Footer = () => {
         <div className="paddings innerWidth flexCenter f-container">
             {/** left */}
             <div className="flexColStart f-left">
-                <img src="./logo2" alt="" />
+                <img src="./logo2.png" alt=""  width={120}/>
+                <span className="secondaryText"> Our vision is to make all people 
+                <br/>
+                the best place to live for them
+                </span>
             </div>
             {/** right */}
+            <div className="flexColStart f-right">
+                <span className="primaryText">Information</span>
+                <span className="secondaryText">145 New york, FL 4571, USA</span>
+                <div className="flexCenter f-menu">
+                    <span>Property</span>
+                    <span>Services</span>
+                    <span>Products</span>
+                    <span>Contact</span>
+                </div>
+            </div>
         </div>
     </section>
   )
