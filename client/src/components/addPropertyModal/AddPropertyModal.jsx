c33bc0804e8b1d80b71efd81e05d8c06ea882fbd
Ruth Gurung <gurungruth2@gmail.com>
2023-12-05 16:10:38 +0100

    minor test code added

--- client/src/components/addPropertyModal/AddPropertyModal.jsx
+++ client/src/components/addPropertyModal/AddPropertyModal.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import {Container, Modal, Stepper} from "@mantine/core"
+import { Container, Modal, Stepper } from "@mantine/core"
 import AddLocation from "../addLocation/AddLocation";
 import UploadImage from "../uploadImage/UploadImage"
 import BasicDetails from "../basicDetails/BasicDetails";
 import Facilities from "../facilities/Facilities";
 
-const AddPropertyModal = ({opened, setOpened}) => {
+const AddPropertyModal = ({ opened, setOpened }) => {
     const [active, setActive] = useState(0);
-    const {user} = useAuth0();
+    const { user } = useAuth0();
+    console.log('user Email', user.email)
     const [propertyDetails, setPropertyDetails] = useState({
         title: "",
         description: "",
@@ -29,55 +30,55 @@ const AddPropertyModal = ({opened, setOpened}) => {
     const nextStep = () => setActive((current) => (current < 4 ? current + 1 : current));
     const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
-  return (
-    <Modal
-    opened={opened}
-    onClose={() => setOpened(false)}
-    closeOnClickOutside
-    size={"90rem"}
-    >
-       <Container h={"40rem"} w={"100%"}>
-            <Stepper active={active} onStepClick={setActive} breakpoint="sm" allowNextStepsSelect={false}>
-                <Stepper.Step label="Location" description="Address">
-                    <AddLocation
-                        nextStep={nextStep}
-                        propertyDetails={propertyDetails}
-                        setPropertyDetails={setPropertyDetails}
-                    />
-                </Stepper.Step>
-                <Stepper.Step label="Images" description="Upload">
-                    <UploadImage
-                        nextStep={nextStep}
-                        prevStep={prevStep}
-                        propertyDetails={propertyDetails}
-                        setPropertyDetails={setPropertyDetails}
-                    />
-                </Stepper.Step>
-                <Stepper.Step label="Basics" description="Details">
-                    <BasicDetails
-                        nextStep={nextStep}
-                        prevStep={prevStep}
-                        propertyDetails={propertyDetails}
-                        setPropertyDetails={setPropertyDetails}
-                    />
-                </Stepper.Step>
-                 <Stepper.Step label="Facilities" description="Details">
-                    <Facilities
-                        prevStep={prevStep}
-                        propertyDetails={propertyDetails}
-                        setPropertyDetails={setPropertyDetails}
-                        setActive={setActive}
-                        setOpened={setOpened}
+    return (
+        <Modal
+            opened={opened}
+            onClose={() => setOpened(false)}
+            closeOnClickOutside
+            size={"90rem"}
+        >
+            <Container h={"40rem"} w={"100%"}>
+                <Stepper active={active} onStepClick={setActive} breakpoint="sm" allowNextStepsSelect={false}>
+                    <Stepper.Step label="Location" description="Address">
+                        <AddLocation
+                            nextStep={nextStep}
+                            propertyDetails={propertyDetails}
+                            setPropertyDetails={setPropertyDetails}
+                        />
+                    </Stepper.Step>
+                    <Stepper.Step label="Images" description="Upload">
+                        <UploadImage
+                            nextStep={nextStep}
+                            prevStep={prevStep}
+                            propertyDetails={propertyDetails}
+                            setPropertyDetails={setPropertyDetails}
+                        />
+                    </Stepper.Step>
+                    <Stepper.Step label="Basics" description="Details">
+                        <BasicDetails
+                            nextStep={nextStep}
+                            prevStep={prevStep}
+                            propertyDetails={propertyDetails}
+                            setPropertyDetails={setPropertyDetails}
+                        />
+                    </Stepper.Step>
+                    <Stepper.Step label="Facilities" description="Details">
+                        <Facilities
+                            prevStep={prevStep}
+                            propertyDetails={propertyDetails}
+                            setPropertyDetails={setPropertyDetails}
+                            setActive={setActive}
+                            setOpened={setOpened}
 
-                    />
-                </Stepper.Step>
-                <Stepper.Completed>
-                Completed, click back button to get to previous step
-                </Stepper.Completed>
-            </Stepper>
-       </Container>    
-    </Modal>
-  )
+                        />
+                    </Stepper.Step>
+                    <Stepper.Completed>
+                        Completed, click back button to get to previous step
+                    </Stepper.Completed>
+                </Stepper>
+            </Container>
+        </Modal>
+    )
 }
 
 export default AddPropertyModal
