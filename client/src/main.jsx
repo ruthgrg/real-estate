3f7ac84918d1e9d39d408dfc7042847c6c35fe87
Ruth Gurung <gurungruth2@gmail.com>
2023-12-05 11:10:26 +0100

    auth0 updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_DOMAIN}
       clientId={import.meta.env.VITE_CLIENTID}
       authorizationParams={{
-        redirect_uri: "http://127.0.0.1:5173"
+        redirect_uri: "hhttps://real-estate-rho-lovat.vercel.app"
       }}
       audience="http://localhost:8000"
       scope="openid profile email"
